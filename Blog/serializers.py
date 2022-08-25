14e933b3166f3fd5c40dfc1214adee31d421437c
mahdiramazani <mahdiramazani1281@gmail.com>
2022-08-25 11:48:51 +0430

    add model serializers

--- Blog/serializers.py
+++ Blog/serializers.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
 from .models import Article
 
-class ArticleSerializers(serializers.Serializer):
-    id=serializers.IntegerField(required=False)
-    titel=serializers.CharField(max_length=50)
-    text=serializers.CharField(max_length=400)
-    status=serializers.BooleanField()
+# class ArticleSerializers(serializers.Serializer):
+#     id=serializers.IntegerField(required=False)
+#     titel=serializers.CharField(max_length=50)
+#     text=serializers.CharField(max_length=400)
+#     status=serializers.BooleanField()
+#
+#
+#     def create(self, validated_data):
+#
+#         return Article.objects.create(**validated_data)
+
+class ArticleSerializers(serializers.ModelSerializer):
 
 
-    def create(self, validated_data):
-
-        return Article.objects.create(**validated_data)
-
+    class Meta:
+        model=Article
+        fields=["text","titel","status","id"]
+        read_only_fields=("id",)
